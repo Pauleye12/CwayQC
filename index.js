a1f830033c4251a9cca186027c53bd5d15efee19
pauleye <orepaul12@gmail.com>
2023-05-03 07:06:06 +0100

    first Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("hey mami");
+const $form = document.forms.TTA_Inputs;
+// console.log($form)
+
+const $lacticBtn = document.getElementById("lactic");
+
+const $citricBtn = document.getElementById("citric");
+
+const $dispAns = document.querySelector(".dispAns")
+
+const calcTTA = (val) => {
+  return (e) => {
+    e.preventDefault();
+    const $SampleWeight = $form.sampleWeight.value;
+
+    const $InitialVolume = $form.initialVol.value;
+
+    const $FinalVol = $form.finalVol.value;
+
+    const volDiff = ($InitialVolume - $FinalVol) / $SampleWeight;
+    if (typeof volDiff == "number") {
+      const Ans = 0.1 * val * volDiff * 100;
+        console.log(Ans);
+        $dispAns.innerHTML= `<p>= ${Ans}</p>`
+      return Ans;
+    } else {
+      console.log("not a number");
+    }
+  };
+};
+
+$lacticBtn.addEventListener("click", calcTTA(0.09));
+$citricBtn.addEventListener("click", calcTTA(0.0641));
